1b6304114a79252bce0f523efe431409d6d378f5
Nathaniyell <essien.nathan@yahoo.com>
2025-01-31 14:41:14 +0100

    Update app structure and configuration, remove unused files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase', '@pinia/nuxt', '@vite-pwa/nuxt', 'nuxt-icon'],
+  modules: ['@nuxtjs/tailwindcss', 
+    //'@nuxtjs/supabase',
+     '@pinia/nuxt',
+      '@vite-pwa/nuxt',
+       'nuxt-icon'],
   runtimeConfig: {
     public: {
       supabaseUrl: process.env.SUPABASE_URL,
