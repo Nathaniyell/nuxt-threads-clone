77b2854d57946481bac5e3ba2d39bd69741f7d03
Nathaniyell <essien.nathan@yahoo.com>
2025-01-31 14:35:02 +0100

    Initial Commit / setup project files

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/supabase', '@pinia/nuxt', '@vite-pwa/nuxt', 'nuxt-icon'],
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_KEY,
+      bucketUrl: process.env.BUCKET_URL,
+    }
+  },
+  devtools: { enabled: true }
+})
