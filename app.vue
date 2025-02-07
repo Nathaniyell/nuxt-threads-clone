93e2b27ddd584197fde4da1acdeb06caf3b72150
Nathaniyell <essien.nathan@yahoo.com>
2025-02-07 22:58:30 +0100

    Integrate CreatePost component with user store menu overlay

--- app.vue
+++ app.vue
@@ -1,6 +1,23 @@
+<script setup>
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore();
+</script>
+
+
 <template>
   <div class="bg-black fixed w-screen h-screen">
     <NuxtRouteAnnouncer />
     <NuxtPage />
+    <CreatePost
+    :class="[
+      {
+        'max-h-[100vh] transition-all duration-200 ease-in visible': userStore.isMenuOverlay
+      },
+      {
+        'max-h-0 transition-all duration-200 ease-out invisible': !userStore.isMenuOverlay
+      }
+    ]"
+    
+    />
   </div>
 </template>
