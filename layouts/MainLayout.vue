0b19d78666b58d58898fea0da230c9967b37d103
Nathaniyell <essien.nathan@yahoo.com>
2025-02-07 21:04:23 +0100

    Refactor bottom navigation menu icons and styling

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { Icon } from '@iconify/vue'
+import { useUserStore } from '../stores/user'
+const userStore = useUserStore()
 </script>
 
 <template>
@@ -7,7 +9,6 @@ import { Icon } from '@iconify/vue'
         <div class="w-full max-w-[500px] mx-auto">
             <div id="TopMenu" class="flex justify-center items-center w-full px-2 h-10 my-2">
                 <img src="/threads-logo.png" alt="logo" class="w-[35px]">
-
             </div>
         </div>
         <div class="w-full max-w-[500px] mx-auto flex h-screen">
@@ -15,17 +16,17 @@ import { Icon } from '@iconify/vue'
         </div>
         <div id="BottomMenu" class="fixed z-50 flex w-full px-2 h-[70px] border-t border-t-gray-700 bg-black bottom-0">
             <div class="w-full max-w-[500px] mx-auto flex justify-around items-center">
-                <button @click="navigateTo('/')" class="w-full text-white h-full flex flex-col items-center">
-                    <Icon class="mb-1 text-white" name="mdi:home" size="32" color="#ffffff" />
-                    Home
+                <button @click="navigateTo('/')"
+                    class="w-full text-white h-full flex flex-col items-center justify-center">
+                    <Icon icon="material-symbols:home" width="32" height="32" />
                 </button>
-                <button @click="userStore.isMenuOverlay = true" class="w-full h-full flex flex-col items-center text-white">
-                    <Icon class="mb-1 !text-white" name="mdi:pencil-box-outline" size="32" color="#ffffff" />
-                    Edit
+                <button @click="userStore.isMenuOverlay = true"
+                    class="w-full h-full text-white flex flex-col items-center justify-center">
+                    <Icon icon="material-symbols:edit-square-outline-rounded" width="32" height="32" />
                 </button>
-                <button @click="userStore.isLogoutOverlay = true" class="w-full h-full flex flex-col items-center text-white">
-                    <Icon class="mb-1 text-white" name="mdi:logout" size="32" color="#ffffff" />
-                    Logout  
+                <button @click="userStore.isLogoutOverlay = true"
+                    class="w-full h-full text-white flex flex-col items-center justify-center">
+                    <Icon icon="material-symbols:logout" width="32" height="32" />
                 </button>
             </div>
         </div>
